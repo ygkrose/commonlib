2ca2d20165034b567887c7ca2a631bfd01f85580
jackyang <jackyang@mail.newcity.com.tw>
2020-06-11 17:02:29 +0800

    必要模組

---
+++ DataLayer/NCORM/Model/TokenClaim.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewCity.DataAccess.Model
+{
+    /// <summary>
+    /// 權杖資訊類別
+    /// </summary>
+    public class TokenClaim
+    {
+        public string UserID { get; set; }
+        public string UserCode { get; set; }
+        public string CompanyCode { get; set; }
+
+        public string ProjectCode { get; set; }
+    }
+}
